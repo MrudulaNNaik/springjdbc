2720734b5530a16a97a9ed6fa83712e855a35569
MRUDULA N NAIK <mrudulannaik1999@gmail.com>
2021-04-22 17:21:10 +0530

    injecting dependencies using string

--- springjdbc/src/basic/dao/StudentDaoImpl.java
+++ springjdbc/src/basic/dao/StudentDaoImpl.java
@@ -8,8 +8,13 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import basic.model.Student;
 
 public class StudentDaoImpl  implements StudentDao{
-	private JdbcTemplate jdbcTemplate = new JdbcTemplate(getDataSource());
-
+	private JdbcTemplate jdbcTemplate ;
+	
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
+	
 	@Override
 	public void insert(Student student) {
 		
@@ -18,14 +23,6 @@ public class StudentDaoImpl  implements StudentDao{
 
 		int no_rows_inserted =	jdbcTemplate.update(sql,objects);
 		System.out.println("no of rows inserted  is "+ no_rows_inserted);
-	}
-
-	public DataSource getDataSource() {
-		String url = "jdbc:mysql://127.0.0.1:3306/spring_jdbc";
-		String username = "root";
-		String password ="";
-
-		DriverManagerDataSource dataSource = new DriverManagerDataSource(url, username, password);
-		return dataSource;
+	
 	}
 }
